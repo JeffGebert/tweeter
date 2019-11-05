70b03a9b6c16e84930cf73c306e4a30a2b81b260
Jeff Gebert <jeffgebert@hotmail.com>
2019-11-05 21:15:59 +0000

    added character count on tweet input box

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  console.log("ready bitches");
+  $('textarea').on('keyup', function() {
+    let tweetLength = 140 - $(this).val().length;
+
+    if (tweetLength < 0) {
+      $(this).next().children('span').text((140 - $(this).val().length));
+      $(this).next().children('span').attr('class', 'redCounter');
+
+    } else {
+      $(this).next().children('span').text((140 - $(this).val().length));
+      $(this).next().children('span').attr('class', 'counter');
+
+    }
+  });
+  
+});
